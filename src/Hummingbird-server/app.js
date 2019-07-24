15d62338ae5c07be429f4741115c96270d4c1f8d
StephenChips <tcw6991@gmail.com>
2019-07-24 20:10:36 +0800

    *Still messy*, finish dividing static html files into mustaches files.
    Relative page:
    1. Home page, which shows all articles of the blog ordering by publish date.
    2. Section's article page, which shows all of its artcles.
    3. Section's category page, which shows all of its categories.
    4. Section's article-category page, which only show all articles that belong to a category and that sections.
    5. Login page
    6. Aritcle page, which shows the article content.
    7. About page, which shows some 'about' information of the blog and the blog's owner.
    
    TODO next:
    1. Write some simple data mocking utilities so that I can get rid of writing ugly and lengthily mock data at the router.
    2. Remove redundant style codes and divide them into files by modules.

--- src/Hummingbird-server/app.js
+++ src/Hummingbird-server/app.js
@@ -4,22 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var pageRouter = require('./routes/pages');
 var apiRouter = require('./routes/api');
 
-var app = express();
+var mustacheExpress = require('mustache-express');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var app = express();
+var VIEW_PATH = __dirname + '/views';
+var VIEW_TEMPLATE_EXT = 'mst'; // View template's extension
+
+// Register '.mustache' extension with The Mustache Express
+app.engine(VIEW_TEMPLATE_EXT, mustacheExpress(VIEW_PATH + '/partials', '.' + VIEW_TEMPLATE_EXT));
+app.set('view engine', VIEW_TEMPLATE_EXT);
+app.set('views', VIEW_PATH);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', pageRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
